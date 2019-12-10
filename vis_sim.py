2530a898376ee156061e60de063f32882b1c086c
haydenm2 <haydenmmorgan@gmail.com>
2019-12-10 10:19:46 -0700

    python sim interface complete

--- vis_sim.py
+++ vis_sim.py
@@ -1,149 +1,176 @@
-#!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-
 
 def rot3dxp(ang):
     R = np.array([[1, 0, 0], [0, np.cos(ang), -np.sin(ang)], [0, np.sin(ang), np.cos(ang)]]).transpose()
     return R
 
-
 def rot3dyp(ang):
     R = np.array([[np.cos(ang), 0, np.sin(ang)], [0, 1, 0], [-np.sin(ang), 0, np.cos(ang)]]).transpose()
     return R
-
 
 def rot3dzp(ang):
     R = np.array([[np.cos(ang), -np.sin(ang), 0], [np.sin(ang), np.cos(ang), 0], [0, 0, 1]]).transpose()
     return R
 
+class UAV_simulator:
+    def __init__(self, x0=np.array([[-10], [0], [40]]), h_fov=np.deg2rad(45), v_fov=np.deg2rad(45)):
 
-if __name__ == "__main__":
-    # UAV and target conditions
-    r = 10
-    h = 40
-    x = np.array([[-r], [0], [h]])
-    xt = np.array([[0], [0], [0]])
-    rpy = np.array([20, 10, 10])
-    rpyg = np.array([20, 10, 10])
+        # UAV and target conditions
+        self.x = x0     # UAV state
+        self.xt = np.array([[0], [0], [0]])     # target state
+        self.rpy = np.array([np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)])   # UAV attitude (roll, pitch, yaw)
+        self.ypr_g = np.array([np.deg2rad(0), np.deg2rad(0), np.deg2rad(0)])   # gimbal attitude (roll, pitch, yaw)
+        self.h_fov = h_fov     # horizontal field of view
+        # phir = self.h_fov / 2 - self.ypr_g[0] + np.arctan2(np.sign(self.x[0]) * r, self.x[2])  # right critical constraint
+        # phil = -self.h_fov / 2 - self.ypr_g[0] + np.arctan2(np.sign(self.x[0]) * r, self.x[2])  # left critical constraint
+        self.v_fov = v_fov     # vertical field of view
+        # thetab = self.v_fov / 2 - self.ypr_g[1]  # back critical constraint
+        # thetaf = -self.v_fov / 2 - self.ypr_g[1]  # front critical constraint
 
-    phi_fov = np.deg2rad(45)
-    phi_g = np.deg2rad(rpyg[0])
-    phir = phi_fov / 2 - phi_g + np.arctan2(np.sign(x[0]) * r, h)  # right critical constraint
-    phil = -phi_fov / 2 - phi_g + np.arctan2(np.sign(x[0]) * r, h)  # left critical constraint
+        # General
+        self.uav_length = 7
+        self.x_line_scale = 10
+        self.e1 = np.array([[1], [0], [0]])  # basis vector 1
+        self.e2 = np.array([[0], [1], [0]])  # basis vector 2
+        self.e3 = np.array([[0], [0], [1]])  # basis vector 3
+        plt_range = 100
 
-    phi = np.deg2rad(rpy[0])
+        # Setup plotting base
+        fig = plt.figure(1)
+        self.ax = plt.axes(projection='3d')     # plot axis handle
+        self.ax.set_xlim(-plt_range, plt_range)      # plotting x limit
+        self.ax.set_ylim(-plt_range, plt_range)      # plotting y limit
+        self.ax.set_zlim(-2, plt_range)      # plotting z limit
+        self.ax.set_title('3D Sim')      # plotting title
+        self.ax.set_xlabel('x')      # plotting x axis label
+        self.ax.set_ylabel('y')      # plotting y axis label
+        self.ax.set_zlabel('z')      # plotting z axis label
+        self.ax.grid(True)      # plotting show grid lines
 
-    theta_fov = np.deg2rad(45)
-    theta_g = np.deg2rad(rpyg[1])
-    thetab = theta_fov / 2 - theta_g  # back critical constraint
-    thetaf = -theta_fov / 2 - theta_g  # front critical constraint
+        # Define initial rotation matrices
+        self.R_iv = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])      # rotation matrix from inertial to vehicle frame
+        self.R_vb = rot3dzp(self.rpy[2]) @ rot3dyp(self.rpy[1]) @ rot3dxp(self.rpy[0])      # rotation matrix from vehicle to body frame
+        self.R_bg = rot3dzp(self.ypr_g[0]) @ rot3dyp(self.ypr_g[1]) @ rot3dxp(self.ypr_g[2])      # rotation matrix from body to gimbal frame
+        self.R_gc = np.eye(3)      # rotation matrix from gimbal to camera frame
+        self.R_cfov1_x = rot3dyp(self.h_fov / 2)      # rotation matrix from camera to max x field of view frame
+        self.R_cfov2_x = rot3dyp(-self.h_fov / 2)      # rotation matrix from camera to min x field of view frame
+        self.R_cfov1_y = rot3dxp(self.v_fov / 2)      # rotation matrix from camera to max y field of view frame
+        self.R_cfov2_y = rot3dxp(-self.v_fov / 2)      # rotation matrix from camera to min y field of view frame
 
-    theta = np.deg2rad(rpy[1])
+        # Rotate animation lines and points back out to common inertial frame for plotting
+        llos = np.hstack([self.x, self.xt])     # line of sight vector
+        lx = np.hstack((np.zeros((3, 1)), self.e1))*self.x_line_scale
+        lquiv = np.hstack((np.zeros((3, 1)), self.e1)) * self.uav_length
+        lquiv = self.R_iv.transpose() @ self.R_vb.transpose() @ lquiv + self.x      # transformed UAV heading arrow
+        lcamx = self.R_iv.transpose() @ self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ lx + self.x      # transformed camera x-axis line
+        lbNorth = self.R_iv.transpose() @ lx + self.x      # transformed North heading line
+        L = np.hstack((self.R_cfov1_y.transpose() @ self.R_cfov1_x.transpose() @ self.e3,
+                       self.R_cfov2_y.transpose() @ self.R_cfov1_x.transpose() @ self.e3,
+                       self.R_cfov2_y.transpose() @ self.R_cfov2_x.transpose() @ self.e3,
+                       self.R_cfov1_y.transpose() @ self.R_cfov2_x.transpose() @ self.e3))       # field of view corner lines
+        L_v = self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ L                # field of view corner lines in vehicle frame
+        pts = self.R_iv @ (self.x[2] * L_v @ np.linalg.inv(np.diag((self.e3.transpose() @ L_v)[0]))) + self.x     # field of view corners projected on observation plane
+        lfov1 = np.hstack((self.x, pts[:, 0].reshape(-1, 1)))       # field of view corner line 1
+        lfov2 = np.hstack((self.x, pts[:, 1].reshape(-1, 1)))       # field of view corner line 2
+        lfov3 = np.hstack((self.x, pts[:, 2].reshape(-1, 1)))       # field of view corner line 3
+        lfov4 = np.hstack((self.x, pts[:, 3].reshape(-1, 1)))       # field of view corner line 4
 
-    psi = np.deg2rad(rpy[2])
-    psi_g = np.deg2rad(rpyg[2])
+        l_temp = self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ self.e3
+        pts_temp = self.R_iv @ (self.x[2] * l_temp @ np.linalg.inv(np.diag((self.e3.transpose() @ l_temp)[0]))) + self.x
+        loptax = np.hstack((self.x, pts_temp.reshape(-1, 1)))       # optical axis line
 
-    xlos = h * np.tan(-phi - phi_g)
-    rx = (xlos + h * np.tan(phi_fov / 2)) / (1 - xlos * np.tan(phi_fov / 2) / h)
-    lx = (xlos - h * np.tan(phi_fov / 2)) / (1 + xlos * np.tan(phi_fov / 2) / h)
+        l_temp = self.R_vb.transpose() @ self.e3
+        pts_temp = self.R_iv @ (self.x[2] * l_temp @ np.linalg.inv(np.diag((self.e3.transpose() @ l_temp)[0]))) + self.x
+        lbz = np.hstack((self.x, pts_temp.reshape(-1, 1)))       # z-axis body frame line
 
-    ylos = h * np.tan(theta + theta_g)
-    by = (ylos + h * np.tan(theta_fov / 2)) / (1 - ylos * np.tan(theta_fov / 2) / h)
-    fy = (ylos - h * np.tan(theta_fov / 2)) / (1 + ylos * np.tan(theta_fov / 2) / h)
+        # Plotting handles
+        self.plos = self.ax.plot3D(llos[0, :], llos[1, :], llos[2, :])  # plot line of sight vector
+        self.pose_point = self.ax.plot3D(self.x[0], self.x[1], self.x[2], 'ko', MarkerFaceColor='k')  # plot point at UAV pose
+        self.pose_line = self.ax.plot3D([self.x[0], self.x[0]], [self.x[1], self.x[1]], [self.x[2], 0], 'k--')  # plot UAV pose line perpendicular to observation plane
+        self.pose_target = self.ax.plot3D(self.xt[0], self.xt[1], self.xt[2], 'ro', MarkerFaceColor='r')  # plot point at target pose
+        self.ploptax = self.ax.plot3D(loptax[0, :], loptax[1, :], loptax[2, :], 'r--')       # plot optical axis line
+        self.plfov1 = self.ax.plot3D(lfov1[0, :], lfov1[1, :], lfov1[2, :], 'r-')       # plot field of view line 1
+        self.plfov2 = self.ax.plot3D(lfov2[0, :], lfov2[1, :], lfov2[2, :], 'r-')       # plot field of view line 2
+        self.plfov3 = self.ax.plot3D(lfov3[0, :], lfov3[1, :], lfov3[2, :], 'r-')       # plot field of view line 3
+        self.plfov4 = self.ax.plot3D(lfov4[0, :], lfov4[1, :], lfov4[2, :], 'r-')       # plot field of view line 4
+        self.plcamx = self.ax.plot3D(lcamx[0, :], lcamx[1, :], lcamx[2, :], 'r-')       # plot camera x axis
+        self.plbNorth = self.ax.plot3D(lbNorth[0, :], lbNorth[1, :], lbNorth[2, :], 'g-')       # plot North heading line on body
+        self.plbz = self.ax.plot3D(lbz[0, :], lbz[1, :], lbz[2, :], 'k-')       # plot z-axis body frame line
+        plpts = np.hstack((pts, pts[:, 0].reshape(-1, 1)))       # field of view extent points (with redundant final point)
+        self.pfov = self.ax.plot3D(plpts[0, :], plpts[1, :], plpts[2, :], 'c-')       # plot field of view polygon projection on observation plane
+        self.quiv = self.ax.plot3D(lquiv[0, :], lquiv[1, :], lquiv[2, :], 'b-', LineWidth=5)       # plot UAV arrow
 
-    # general plotting
-    linedist = 100
-    plt_rng = 100
-    uav_length = 15
-    endpoint = np.array([[0], [-linedist]])
+        self.UpdatePlot()
 
-    #  ######################################################################
-    #  #############################3D PLOT##################################
-    #  ######################################################################
+    def UpdateX(self, x):
+        self.x = x.reshape(-1, 1)
 
-    # setup background base
-    fig = plt.figure(1)
-    ax = plt.axes(projection='3d')
-    los = np.hstack([x, xt])
-    ax.plot3D(los[0, :], los[1, :], los[2, :])
-    pose_point = ax.plot3D(x[0], x[1], x[2], 'ko', MarkerFaceColor='k')
-    pose_line = ax.plot3D([x[0], x[0]], [x[1], x[1]], [x[2], 0], 'k--')
-    pose_target = ax.plot3D(xt[0], xt[1], xt[2], 'ro', MarkerFaceColor='r')
-    lfovbase = np.array([[0], [0], [1]])
-    lfov = np.array([[0, 0], [0, 0], [0, 200]])
-    lquiv = np.array([[-uav_length/2, uav_length/2], [0, 0], [0, 0]])
-    lcamx = np.array([[0, 10], [0, 0], [0, 0]])
-    lbNorth = np.array([[0, 10], [0, 0], [0, 0]])
-    e1 = np.array([[1], [0], [0]])
-    e2 = np.array([[0], [1], [0]])
-    e3 = np.array([[0], [0], [1]])
+    def UpdateTargetX(self, xt):
+        self.xt = xt.reshape(-1, 1)
 
-    ax.set_xlim(-plt_rng, plt_rng)
-    ax.set_ylim(-plt_rng, plt_rng)
-    ax.set_zlim(-2, plt_rng)
-    ax.set_title('3D Sim')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    ax.grid(True)
+    def UpdateRPY(self, rpy):
+        self.rpy = rpy
+        self.R_vb = rot3dzp(self.rpy[2]) @ rot3dyp(self.rpy[1]) @ rot3dxp(self.rpy[0])  # rotation matrix from vehicle to body frame
 
-    Riv = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
-    R_ib = rot3dzp(psi) @ rot3dyp(theta) @ rot3dxp(phi) @ Riv
-    R_vb = rot3dzp(psi) @ rot3dyp(theta) @ rot3dxp(phi)
-    R_bg = rot3dzp(psi_g) @ rot3dyp(theta_g) @ rot3dxp(phi_g)
-    R_gc = np.eye(3)
-    R_cfov1_x = rot3dyp(phi_fov / 2)
-    R_cfov2_x = rot3dyp(-phi_fov / 2)
-    R_cfov1_y = rot3dxp(theta_fov / 2)
-    R_cfov2_y = rot3dxp(-theta_fov / 2)
+    def UpdateGimbalYPR(self, ypr_g):
+        self.ypr_g = ypr_g
+        self.R_bg = rot3dzp(self.ypr_g[0]) @ rot3dyp(self.ypr_g[1]) @ rot3dxp(self.ypr_g[2])      # rotation matrix from body to gimbal frame
 
-    # Rotate animation lines back out to common inertial frame for plotting
-    lquiv = R_ib.transpose() @ lquiv + x
-    lcamx = R_ib.transpose() @ R_bg.transpose() @ R_gc.transpose() @ lcamx + x
-    lbNorth = Riv.transpose() @ lbNorth + x
+    def UpdateHFOV(self, h_fov):
+        self.h_fov = h_fov
+        self.R_cfov1_x = rot3dyp(self.h_fov / 2)  # rotation matrix from camera to max x field of view frame
+        self.R_cfov2_x = rot3dyp(-self.h_fov / 2)  # rotation matrix from camera to min x field of view frame
 
-    L = np.hstack((R_cfov1_y.transpose() @ R_cfov1_x.transpose() @ lfovbase,
-                   R_cfov2_y.transpose() @ R_cfov1_x.transpose() @ lfovbase,
-                   R_cfov2_y.transpose() @ R_cfov2_x.transpose() @ lfovbase,
-                   R_cfov1_y.transpose() @ R_cfov2_x.transpose() @ lfovbase))
-    L_v = R_vb.transpose() @ R_bg.transpose() @ R_gc.transpose() @ L
-    pts = Riv @ (h * L_v @ np.linalg.inv(np.diag((e3.transpose() @ L_v)[0]))) + x
-    lfov1 = np.hstack((x, pts[:, 0].reshape(-1, 1)))
-    lfov2 = np.hstack((x, pts[:, 1].reshape(-1, 1)))
-    lfov3 = np.hstack((x, pts[:, 2].reshape(-1, 1)))
-    lfov4 = np.hstack((x, pts[:, 3].reshape(-1, 1)))
+    def UpdateVFOV(self, v_fov):
+        self.v_fov = v_fov
+        self.R_cfov1_y = rot3dxp(self.v_fov / 2)  # rotation matrix from camera to max y field of view frame
+        self.R_cfov2_y = rot3dxp(-self.v_fov / 2)  # rotation matrix from camera to min y field of view frame
 
-    l_temp = R_vb.transpose() @ R_bg.transpose() @ R_gc.transpose() @ lfovbase
-    pts_temp = Riv @ (h * l_temp @ np.linalg.inv(np.diag((e3.transpose() @ l_temp)[0]))) + x
-    loptax = np.hstack((x, pts_temp.reshape(-1, 1)))
+    def UpdatePlot(self):
+        # Rotate animation lines and points back out to common inertial frame for plotting
+        llos = np.hstack([self.x, self.xt])  # line of sight vector
+        lx = np.hstack((np.zeros((3, 1)), self.e1))*self.x_line_scale
+        lquiv = np.hstack((np.zeros((3, 1)), self.e1)) * self.uav_length
+        lquiv = self.R_iv.transpose() @ self.R_vb.transpose() @ lquiv + self.x  # transformed UAV heading arrow
+        lcamx = self.R_iv.transpose() @ self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ lx + self.x  # transformed camera x-axis line
+        lbNorth = self.R_iv.transpose() @ lx + self.x  # transformed North heading line
+        L = np.hstack((self.R_cfov1_y.transpose() @ self.R_cfov1_x.transpose() @ self.e3,
+                       self.R_cfov2_y.transpose() @ self.R_cfov1_x.transpose() @ self.e3,
+                       self.R_cfov2_y.transpose() @ self.R_cfov2_x.transpose() @ self.e3,
+                       self.R_cfov1_y.transpose() @ self.R_cfov2_x.transpose() @ self.e3))  # field of view corner lines
+        L_v = self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ L  # field of view corner lines in vehicle frame
+        pts = self.R_iv @ (self.x[2] * L_v @ np.linalg.inv(np.diag((self.e3.transpose() @ L_v)[0]))) + self.x  # field of view corners projected on observation plane
+        plpts = np.hstack((pts, pts[:, 0].reshape(-1, 1)))  # field of view extent points (with redundant final point)
+        lfov1 = np.hstack((self.x, pts[:, 0].reshape(-1, 1)))  # field of view corner line 1
+        lfov2 = np.hstack((self.x, pts[:, 1].reshape(-1, 1)))  # field of view corner line 2
+        lfov3 = np.hstack((self.x, pts[:, 2].reshape(-1, 1)))  # field of view corner line 3
+        lfov4 = np.hstack((self.x, pts[:, 3].reshape(-1, 1)))  # field of view corner line 4
 
-    l_temp = R_vb.transpose() @ lfovbase
-    pts_temp = Riv @ (h * l_temp @ np.linalg.inv(np.diag((e3.transpose() @ l_temp)[0]))) + x
-    lbz = np.hstack((x, pts_temp.reshape(-1, 1)))
+        l_temp = self.R_vb.transpose() @ self.R_bg.transpose() @ self.R_gc.transpose() @ self.e3
+        pts_temp = self.R_iv @ (self.x[2] * l_temp @ np.linalg.inv(np.diag((self.e3.transpose() @ l_temp)[0]))) + self.x
+        loptax = np.hstack((self.x, pts_temp.reshape(-1, 1)))  # optical axis line
 
-    # # simple rotation FOV lines
-    # loptax = R_ib.transpose() @ R_bg.transpose() @ lfov + x
-    # lfov1 = R_ib.transpose() @ R_bg.transpose() @ R_gc.transpose() @ R_cfov1_y.transpose() @ R_cfov1_x.transpose() @ lfov + x
-    # lfov2 = R_ib.transpose() @ R_bg.transpose() @ R_gc.transpose() @ R_cfov2_y.transpose() @ R_cfov1_x.transpose() @ lfov + x
-    # lfov3 = R_ib.transpose() @ R_bg.transpose() @ R_gc.transpose() @ R_cfov1_y.transpose() @ R_cfov2_x.transpose() @ lfov + x
-    # lfov4 = R_ib.transpose() @ R_bg.transpose() @ R_gc.transpose() @ R_cfov2_y.transpose() @ R_cfov2_x.transpose() @ lfov + x
-    # lbz = R_ib.transpose() @ lfov + x
+        l_temp = self.R_vb.transpose() @ self.e3
+        pts_temp = self.R_iv @ (self.x[2] * l_temp @ np.linalg.inv(np.diag((self.e3.transpose() @ l_temp)[0]))) + self.x
+        lbz = np.hstack((self.x, pts_temp.reshape(-1, 1)))  # z-axis body frame line
 
-    ploptax = ax.plot3D(loptax[0, :], loptax[1, :], loptax[2, :], 'r--')
-    plfov1 = ax.plot3D(lfov1[0, :], lfov1[1, :], lfov1[2, :], 'r-')
-    plfov2 = ax.plot3D(lfov2[0, :], lfov2[1, :], lfov2[2, :], 'r-')
-    plfov3 = ax.plot3D(lfov3[0, :], lfov3[1, :], lfov3[2, :], 'r-')
-    plfov4 = ax.plot3D(lfov4[0, :], lfov4[1, :], lfov4[2, :], 'r-')
-    plcamx = ax.plot3D(lcamx[0, :], lcamx[1, :], lcamx[2, :], 'r-')
-    plbNorth = ax.plot3D(lbNorth[0, :], lbNorth[1, :], lbNorth[2, :], 'g-')
-    plbz = ax.plot3D(lbz[0, :], lbz[1, :], lbz[2, :], 'k-')
+        # Plotting handles
+        self.plos[0].set_data_3d(llos[0, :], llos[1, :], llos[2, :])  # plot line of sight vector
+        self.pose_point[0].set_data_3d(self.x[0], self.x[1], self.x[2])  # plot point at UAV pose
+        self.pose_line[0].set_data_3d([np.asscalar(self.x[0]), np.asscalar(self.x[0])], [np.asscalar(self.x[1]), np.asscalar(self.x[1])], [np.asscalar(self.x[2]), 0])  # plot UAV pose line perpendicular to observation plane
+        self.pose_target[0].set_data_3d(self.xt[0], self.xt[1], self.xt[2])  # plot point at target pose
+        self.ploptax[0].set_data_3d(loptax[0, :], loptax[1, :], loptax[2, :])  # plot optical axis line
+        self.plfov1[0].set_data_3d(lfov1[0, :], lfov1[1, :], lfov1[2, :])  # plot field of view line 1
+        self.plfov2[0].set_data_3d(lfov2[0, :], lfov2[1, :], lfov2[2, :])  # plot field of view line 2
+        self.plfov3[0].set_data_3d(lfov3[0, :], lfov3[1, :], lfov3[2, :])  # plot field of view line 3
+        self.plfov4[0].set_data_3d(lfov4[0, :], lfov4[1, :], lfov4[2, :])  # plot field of view line 4
+        self.plcamx[0].set_data_3d(lcamx[0, :], lcamx[1, :], lcamx[2, :])  # plot camera x axis
+        self.plbNorth[0].set_data_3d(lbNorth[0, :], lbNorth[1, :], lbNorth[2, :])  # plot North heading line on body
+        self.plbz[0].set_data_3d(lbz[0, :], lbz[1, :], lbz[2, :])  # plot z-axis body frame line
+        self.pfov[0].set_data_3d(plpts[0, :], plpts[1, :], plpts[2, :])  # plot field of view polygon projection on observation plane
+        self.quiv[0].set_data_3d(lquiv[0, :], lquiv[1, :], lquiv[2, :])  # plot UAV arrow
 
-    plpts = np.hstack((pts, pts[:, 0].reshape(-1, 1)))
-    pfov = ax.plot3D(plpts[0, :], plpts[1, :], plpts[2, :], 'c-')
-
-    ax.quiver3D(lquiv[0, 0], lquiv[1, 0], lquiv[2, 0], lquiv[0, 1] - lquiv[0, 0], lquiv[1, 1] - lquiv[1, 0], lquiv[2, 1] - lquiv[2, 0], 'b', LineWidth=5)
-
-    plt.show()
+        self.ax.autoscale_view()
+        plt.pause(0.01)
---
+++ vis_sim_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from vis_sim import UAV_simulator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    # init simulator
+    sim = UAV_simulator(np.array([[-10], [0], [40]]))
+
+    # example of cycling through UAV orientations
+    for i in range(0, 20, 1):
+        sim.UpdateRPY(np.array((np.deg2rad(i), np.deg2rad(0), np.deg2rad(0))))
+        sim.UpdatePlot()
+    for j in range(0, 20, 1):
+        sim.UpdateRPY(np.array((np.deg2rad(i), np.deg2rad(j), np.deg2rad(0))))
+        sim.UpdatePlot()
+    for k in range(0, 20, 1):
+        sim.UpdateRPY(np.array((np.deg2rad(i), np.deg2rad(j), np.deg2rad(k))))
+        sim.UpdatePlot()
+
+    # example of cycling through gimbal orientations
+    for i in range(0, -10, -1):
+        sim.UpdateGimbalYPR(np.array((np.deg2rad(i), np.deg2rad(0), np.deg2rad(0))))
+        sim.UpdatePlot()
+    for j in range(0, -10, -1):
+        sim.UpdateGimbalYPR(np.array((np.deg2rad(i), np.deg2rad(j), np.deg2rad(0))))
+        sim.UpdatePlot()
+    for k in range(0, -10, -1):
+        sim.UpdateGimbalYPR(np.array((np.deg2rad(i), np.deg2rad(j), np.deg2rad(k))))
+        sim.UpdatePlot()
+
+    # example of cycling through UAV positions
+    for i in range(-10, 20, 1):
+        sim.UpdateX(np.array((i, 0, 40)))
+        sim.UpdatePlot()
+    for j in range(0, 20, 1):
+        sim.UpdateX(np.array((i, j, 40)))
+        sim.UpdatePlot()
+    for k in range(40, 80, 1):
+        sim.UpdateX(np.array((i, j, k)))
+        sim.UpdatePlot()
+
+    # example of cycling through target positions
+    for i in range(0, -10, -1):
+        sim.UpdateTargetX(np.array((i, 0, 0)))
+        sim.UpdatePlot()
+    for j in range(0, 10, 1):
+        sim.UpdateTargetX(np.array((i, j, 0)))
+        sim.UpdatePlot()
+    for k in range(0, 10, 1):
+        sim.UpdateTargetX(np.array((i, j, k)))
+        sim.UpdatePlot()
+
+    # example of cycling through camera FOV angles
+    for i in range(45, 15, -1):
+        sim.UpdateHFOV(np.deg2rad(i))
+        sim.UpdatePlot()
+    for i in range(45, 20, -1):
+        sim.UpdateVFOV(np.deg2rad(i))
+        sim.UpdatePlot()
+
+    plt.show()
